f6d4e880603e006c050cecddcf6c34cbd39cc5f9
Felipe Santana <felipe.jesus0801@gmail.com>
2023-12-26 14:55:10 -0300

    feat: creating get appointments by date

--- server/src/repositories/AppointmentRepository.ts
+++ server/src/repositories/AppointmentRepository.ts
@@ -2,8 +2,9 @@ import { type Appointment, type Prisma } from '@prisma/client'
 
 export interface AppointmentRepository {
   create: (data: Prisma.AppointmentUncheckedCreateInput) => Promise<Appointment>
-  findAll: (doctorId: string) => Promise<Appointment[] | null>
-  // findByDate: (date: Date) => Promise<Appointment | null>
   findByDoctorId: (doctorId: string) => Promise<Appointment[] | null>
+
+  findAll: (doctorId: string) => Promise<Appointment[] | null>
+  findByDate: (date: Date, doctorId:string) => Promise<Appointment[] | null>
   findOverlappingAppointment: (doctorId: string, startTime: Date, endTime: Date) => Promise<Appointment[] | null>
 }
